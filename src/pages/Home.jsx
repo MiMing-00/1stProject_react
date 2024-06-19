2188ca4d9412811ee7a8a12410ba5d2eb2acbe10
Sera Choi <c.sera2511@gmail.com>
2024-06-19 21:44:25 +0900

    세 분류로 나눔

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return;
+  <>
+    <div>첫번째 파트 (컴포넌트 불러오기)</div>
+    <div>두번째 파트 (컴포넌트 불러오기)</div>
+    <div>세번째 파트 (컴포넌트 불러오기)</div>
+  </>;
 };
 
 export default Home;
