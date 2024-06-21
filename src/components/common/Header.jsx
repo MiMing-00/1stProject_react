0f95f8d4e685406dfa85fbda729727b370915758
Sera Choi <c.sera2511@gmail.com>
2024-06-21 21:26:59 +0900

    [add] : mockup

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="flex fixed w-full top-0 left-0 bg-gray-800 text-white h-14 p-4">
+    <div className="flex mx-auto fixed w-full max-w-[80%] top-0 left-0 bg-gray-800 text-white h-14 p-4">
       헤더
     </div>
   );
--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -4,10 +4,10 @@ import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
-    <>
+    <div className="flex flex-col max-w-xl bg-blue-200 border-4 mx-auto">
       <Header />
       <Outlet />
-    </>
+    </div>
   );
 };
 
