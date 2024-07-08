3930e3cfb800d25975d835b1dff2a4b5280bd346
Sera Choi <c.sera2511@gmail.com>
2024-07-08 22:34:45 +0900

    [style] : detail mockup

--- src/pages/detail/adoptListDetail/AdoptionDetail.jsx
+++ src/pages/detail/adoptListDetail/AdoptionDetail.jsx
@@ -85,17 +85,70 @@ const AdoptionDetail = () => {
     return <div>erroR!!!!!~~~~~</div>;
   }
 
+  const getAge = (petAge) => {
+    const today = new Date();
+    const year = today.getFullYear();
+
+    return year - parseInt(petAge.slice(0, 4));
+  };
+
+  const getGender = (petGender) => {
+    if ((petGender === "F") | (petGender === "f")) {
+      return "암컷";
+    } else if ((petGender === "M") | (petGender === "m")) {
+      return "수컷";
+    } else {
+      return petGender;
+    }
+  };
+
   return (
-    <div className="border-2 rounded-xl w-3/2">
-      <div className="">
+    <div className="border-2 rounded-xl w-3/2 p-4">
+      <div className="m-4 p-4 border-2 rounded-xl">
         {data.colorCd}의 {data.kindCd}
       </div>
-      <div className="flex justify-center items-center w-[90%]">
+      <div className="flex justify-center items-center w-[40%] mx-auto pb-4">
         <img
           src={data.popfile}
           alt={`${data.desertionNo}의 정보`}
           className="w-full h-full object-cover rounded-xl"
         />
+      </div>
+      <div>
+        <h3 className="p-4 m-4 text-2xl">가족이 되어 주세요!</h3>
+        <table className="border-separate border-spacing-2 border border-slate-400 w-full ...">
+          <tbody>
+            <tr>
+              <th className="border border-slate-300 ...">성별</th>
+              <td className="border border-slate-300 ...">
+                {getGender(data.sexCd)}
+              </td>
+            </tr>
+            <tr>
+              <th className="border border-slate-300 ...">나이</th>
+              <td className="border border-slate-300 ...">
+                {getAge(data.age)} 살
+              </td>
+            </tr>
+            <tr>
+              <th className="border border-slate-300 ...">몸무게</th>
+              <td className="border border-slate-300 ...">{data.weight}</td>
+            </tr>
+            <tr>
+              <th className="border border-slate-300 ...">주의사항</th>
+              <td className="border border-slate-300 ...">
+                {data.specialMark}
+              </td>
+            </tr>
+            <tr>
+              <th className="border border-slate-300 ...">보호소 위치</th>
+              <td className="border border-slate-300 ...">
+                {data.careAddr} 의{" "}
+                <span className="font-bold">{data.careNm}</span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
