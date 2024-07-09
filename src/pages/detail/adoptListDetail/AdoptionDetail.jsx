8e7cfa975184e29cdf160b05da8d0d839e05219b
Sera Choi <c.sera2511@gmail.com>
2024-07-09 23:56:37 +0900

    [build]: swiper Li

--- src/pages/detail/adoptListDetail/AdoptionDetail.jsx
+++ src/pages/detail/adoptListDetail/AdoptionDetail.jsx
@@ -1,81 +1,10 @@
 import { useParams } from "react-router-dom";
 import usePetQuery from "../../../hooks/usePetQuery";
+import { IoCall } from "react-icons/io5";
 
 const AdoptionDetail = () => {
   const { id } = useParams();
   const { data, isPending, error } = usePetQuery(id);
-
-  console.log(data);
-
-  //   age
-  // :
-  // "2022(년생)"
-  // careAddr
-  // :
-  // "경상남도 거제시 사등면 두동로1길 109 (사등면, 한국자원재생공사폐비닐적재장) 거제시유기동물보호소"
-  // careNm
-  // :
-  // "거제시유기동물보호소"
-  // careTel
-  // :
-  // "055-639-6368"
-  // chargeNm
-  // :
-  // "이용휘"
-  // colorCd
-  // :
-  // "흰색.노란색.검은색"
-  // desertionNo
-  // :
-  // "448537202400977"
-  // filename
-  // :
-  // "http://www.animal.go.kr/files/shelter/2024/06/202407051707237_s.jpg"
-  // happenDt
-  // :
-  // "20240705"
-  // happenPlace
-  // :
-  // "사등면사무소"
-  // kindCd
-  // :
-  // "[고양이] 한국 고양이"
-  // neuterYn
-  // :
-  // "N"
-  // noticeEdt
-  // :
-  // "20240715"
-  // noticeNo
-  // :
-  // "경남-거제-2024-00278"
-  // noticeSdt
-  // :
-  // "20240705"
-  // officetel
-  // :
-  // "0556396366"
-  // orgNm
-  // :
-  // "경상남도 거제시"
-  // popfile
-  // :
-  // "http://www.animal.go.kr/files/shelter/2024/06/202407051707237.jpg"
-  // processState
-  // :
-  // "보호중"
-  // sexCd
-  // :
-  // "F"
-  // specialMark
-  // :
-  // "경계있음"
-  // weight
-  // :
-  // "3(Kg)"
-  // [[Prototype]]
-  // :
-  // Object
 
   if (isPending) {
     return <div>로딩 중</div>;
@@ -102,53 +31,79 @@ const AdoptionDetail = () => {
     }
   };
 
+  const happenDay = (petHappenDt) => {
+    return `${petHappenDt.slice(0, 4)}년 ${petHappenDt.slice(
+      5,
+      6
+    )}월 ${petHappenDt.slice(7, 8)}일`;
+  };
+
   return (
-    <div className="border-2 rounded-xl w-3/2 p-4">
-      <div className="m-4 p-4 border-2 rounded-xl">
-        {data.colorCd}의 {data.kindCd}
-      </div>
-      <div className="flex justify-center items-center w-[40%] mx-auto pb-4">
-        <img
-          src={data.popfile}
-          alt={`${data.desertionNo}의 정보`}
-          className="w-full h-full object-cover rounded-xl"
-        />
-      </div>
-      <div>
-        <h3 className="p-4 m-4 text-2xl">가족이 되어 주세요!</h3>
-        <table className="border-separate border-spacing-2 border border-slate-400 w-full ...">
-          <tbody>
-            <tr>
-              <th className="border border-slate-300 ...">성별</th>
-              <td className="border border-slate-300 ...">
-                {getGender(data.sexCd)}
-              </td>
-            </tr>
-            <tr>
-              <th className="border border-slate-300 ...">나이</th>
-              <td className="border border-slate-300 ...">
-                {getAge(data.age)} 살
-              </td>
-            </tr>
-            <tr>
-              <th className="border border-slate-300 ...">몸무게</th>
-              <td className="border border-slate-300 ...">{data.weight}</td>
-            </tr>
-            <tr>
-              <th className="border border-slate-300 ...">주의사항</th>
-              <td className="border border-slate-300 ...">
-                {data.specialMark}
-              </td>
-            </tr>
-            <tr>
-              <th className="border border-slate-300 ...">보호소 위치</th>
-              <td className="border border-slate-300 ...">
-                {data.careAddr} 의{" "}
-                <span className="font-bold">{data.careNm}</span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+    <div className="border-2 rounded-xl w-[90%] p-4 mx-auto">
+      <div className="w-full flex flex-col mx-auto">
+        <div className="m-4 p-4 border-2 rounded-xl">
+          {data.colorCd}의 {data.kindCd}
+        </div>
+        <div className="flex justify-center items-center w-[40%] mx-auto pb-4">
+          <img
+            src={data.popfile}
+            alt={`${data.desertionNo}의 정보`}
+            className="w-full h-full object-cover rounded-xl"
+          />
+        </div>
+        <div>
+          <h3 className="p-4 m-4 text-2xl">가족이 되어 주세요!</h3>
+          <table className="border-separate border-spacing-2 border border-slate-400 w-full text-balanc ...">
+            <tbody>
+              <tr>
+                <th className="border border-slate-300 ...">성별</th>
+                <td className="border border-slate-300 ...">
+                  {getGender(data.sexCd)}
+                </td>
+              </tr>
+              <tr>
+                <th className="border border-slate-300 ...">나이</th>
+                <td className="border border-slate-300 ...">
+                  {getAge(data.age)} 살
+                </td>
+              </tr>
+              <tr>
+                <th className="border border-slate-300 ...">몸무게</th>
+                <td className="border border-slate-300 ...">{data.weight}</td>
+              </tr>
+              <tr>
+                <th className="border border-slate-300e ...">특이사항</th>
+                <td className="border border-slate-300 ...">
+                  {data.specialMark}
+                </td>
+              </tr>
+              <tr>
+                <th className="border border-slate-300 ...">보호소 위치</th>
+                <td className="border border-slate-300 ...">
+                  {data.careAddr} 의{" "}
+                  <div className="font-bold">{data.careNm}</div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
+          <div className="flex flex-col justify-center mt-4">
+            <div>
+              이 아이는 <span>{happenDay(data.happenDt)}</span>에 &apos;
+              {data.happenPlace}&apos; 에서 발견됐어요.
+            </div>
+            <div>
+              털색은 예쁜 {data.colorCd}을 가지고 있고, 나이는{" "}
+              {getAge(data.age)}
+              살이에요.
+            </div>
+            <div>
+              이 아이가 눈에 밟히시는 분은 <IoCall /> {data.officetel} 으로
+              연락해주세요.
+            </div>
+            <div>당신의 신중한 선택을 기다리고 있습니다.</div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/home/components/dailySilder/DailySilder.jsx
+++ src/pages/home/components/dailySilder/DailySilder.jsx
@@ -1,11 +1,65 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 const DailySilder = () => {
   return (
     <>
       <div className="border-4 border-indigo-500 h-[400px] w-full">
-        슬라이드 되는 친구 여기다 넣기
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 4000,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
       </div>
     </>
   );
 };
 
 export default DailySilder;
+
+// .swiper {
+//   width: 100%;
+//   height: 100%;
+// }
+
+// .swiper-slide {
+//   text-align: center;
+//   font-size: 18px;
+//   background: #fff;
+
+//   /* Center slide text vertically */
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// }
+
+// .swiper-slide img {
+//   display: block;
+//   width: 100%;
+//   height: 100%;
+//   object-fit: cover;
+// }
